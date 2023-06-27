0d3b0740c5df829cf743422fbc5197afdfe1db59
thiiagoms <thiagom.devsec@gmail.com>
2023-06-27 08:02:45 -0300

    build: add custom migration config :building_construction:

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,10 +15,27 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
+     * Change default migrations path config
+     *
+     * @return void
+     */
+    private function customMigrations(): void
+    {
+        $migrationsPath = database_path('migrations');
+        $directories    = glob($migrationsPath . '/*', GLOB_ONLYDIR);
+        $paths          = array_merge([$migrationsPath], $directories);
+
+        $this->loadMigrationsFrom($paths);
+    }
+
+    /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        /**
+         * Change default migrations path
+         */
+        $this->customMigrations();
     }
 }
